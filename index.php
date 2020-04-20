eb2e79be64a653339bdc3397193597988874bcc1
daviddukeuk <daviddukeuk@gmail.com>
2020-04-20 15:05:32 +0100

    Fixing Homepage includes to newer format

--- index.php
+++ index.php
@@ -1,30 +1,36 @@
-<?php $page_title = "Welcome";?>
-<?php include($_SERVER['DOCUMENT_ROOT'].'/_includes/header.php'); ?>
+<?php
+require_once($_SERVER['DOCUMENT_ROOT'].'/_includes/_functions.php');
+$page_title = "Welcome";// appended to <title> tag
+$menu = "";// navigation section of this page
 
-	<h2>Welcome to the North of England Rat Society (NERS)</h2>
-	<p>The society was birthed in June 2001 by a diverse group of ratters who were keen to see a democratic rat club representing the interests of rat lovers in the North of England. The society, whilst serious about the rat as an exhibition animal, has always maintained a heavy bias towards the rat as a splendid companion animal. This is reflected in our constitution: </p>
-	<ul>
-		<li>To uphold and encourage the fancy rat as an excellent pet and exhibition animal.</li>
-		<li>To safeguard the interests of the fancy rat, and support the rat fancy in general.</li>
-		<li>To advocate and champion the highest quality of rat care.</li>
-		<li>To advance safe and reputable breeding practices.</li>
-		<li>To adopt and promote the NFRS 'standards of excellence', by which fancy rats should be judged.</li>
-		<li>To encourage and maintain optimum practice in judging and show management.</li>
-		<li>To provide help and support for rat keepers throughout the North of England and surrounding areas.</li>
-	</ul>
-	<div class="row">
-		<div class="col-lg-10 col-md-10 col-sm-10 col-xs-12">
-			<p>Today our membership continues to grow, and as well as regular shows around the region we also produce a quarterly magazine which is packed full of informative and fun rat-related articles. Our shows are not just an event for people who want to exhibit fancy rats, but a warm and generous environment where all those who are enthralled by rats can gather to celebrate them, learn more about them and stock up on rat-related goodies from our shop. We provide catering, children's activities, fun events for the rats, a range of pet classes and a full variety schedule.</p>
-		</div>
-		<div class="col-lg-2 col-md-2 col-sm-2 col-xs-12 text-center">
-			<strong>Find us on:</strong>
-			<p><a href="https://www.facebook.com/neratsociety" target="_blank"><img src="/resources/fb.png" alt="NERS on Facebook" width="50" height="50" border="0" style="margin-bottom:10px;"></a>
-            <a href="https://twitter.com/neratsociety" target="_blank"><img src="/resources/twitter.png" alt="NERS on Twitter" width="48" height="48" border="0" style="margin-bottom:10px;"></a></p>
-		</div>
+inc('header'); // display page header
+?>
+
+<h2>Welcome to the North of England Rat Society (NERS)</h2>
+<p>The society was birthed in June 2001 by a diverse group of ratters who were keen to see a democratic rat club representing the interests of rat lovers in the North of England. The society, whilst serious about the rat as an exhibition animal, has always maintained a heavy bias towards the rat as a splendid companion animal. This is reflected in our constitution: </p>
+<ul>
+	<li>To uphold and encourage the fancy rat as an excellent pet and exhibition animal.</li>
+	<li>To safeguard the interests of the fancy rat, and support the rat fancy in general.</li>
+	<li>To advocate and champion the highest quality of rat care.</li>
+	<li>To advance safe and reputable breeding practices.</li>
+	<li>To adopt and promote the NFRS 'standards of excellence', by which fancy rats should be judged.</li>
+	<li>To encourage and maintain optimum practice in judging and show management.</li>
+	<li>To provide help and support for rat keepers throughout the North of England and surrounding areas.</li>
+</ul>
+<div class="row">
+	<div class="col-lg-10 col-md-10 col-sm-10 col-xs-12">
+		<p>Today our membership continues to grow, and as well as regular shows around the region we also produce a quarterly magazine which is packed full of informative and fun rat-related articles. Our shows are not just an event for people who want to exhibit fancy rats, but a warm and generous environment where all those who are enthralled by rats can gather to celebrate them, learn more about them and stock up on rat-related goodies from our shop. We provide catering, children's activities, fun events for the rats, a range of pet classes and a full variety schedule.</p>
 	</div>
-	<p><strong>Alison Campbell<br>
-	NERS founding member</strong></p>
-	<img src="/resources/front.png" class="img-responsive col p-0 m-0" alt="Lovecraft Afterglow (photo by Lisa Maurin)" />
+	<div class="col-lg-2 col-md-2 col-sm-2 col-xs-12 text-center">
+		<strong>Find us on:</strong>
+		<p><a href="https://www.facebook.com/neratsociety" target="_blank"><img src="/resources/fb.png" alt="NERS on Facebook" width="50" height="50" border="0" class="mb-10"></a>
+		<a href="https://twitter.com/neratsociety" target="_blank"><img src="/resources/twitter.png" alt="NERS on Twitter" width="48" height="48" border="0" class="mb-10"></a></p>
+	</div>
+</div>
+<p><strong>Alison Campbell<br>
+NERS founding member</strong></p>
+<img src="/resources/front.png" class="img-responsive col p-0 m-0" alt="Lovecraft Afterglow (photo by Lisa Maurin)" />
 
-
-<?php include($_SERVER['DOCUMENT_ROOT'].'/_includes/footer.php'); ?>
+<?php
+inc('footer'); // display page footer
+?>

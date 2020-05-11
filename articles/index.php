9962fbcdf9d555a4532352c3e07c51929f4bee09
daviddukeuk <daviddukeuk@gmail.com>
2020-05-11 20:58:53 +0100

    Making columns for Articles Index

--- articles/index.php
+++ articles/index.php
@@ -7,130 +7,133 @@ inc('header'); // display page header
 ?>
 
 <h2>Rat-keeping Articles and Information</h2>
-  
-<article>
-    <h3>Health</h3>
-    <ul>
-        <li><a href="/articles/firstaid/">First aid for rats</a></li>
-        <li><a href="/articles/metacam/">Metacam</a></li>
-        <li><a href="/articles/steroids/">Steroids</a></li>
-        <li><a href="/articles/quarantine/">Quarantining rats</a></li>
-        <li><a href="/articles/vets/">Recommended vets</a></li>
-        <li><a href="/articles/infectionsafepractice/">Infection and safe practice</a></li>
-    </ul>
-</article>
 
-<article>
-    <h3>Training &amp; behaviour</h3>
-    <ul>
-        <li><a href="/articles/agilitytraining/">Agility training</a></li>
-        <li><a href="/articles/training/">Training your rat</a></li>
-    </ul>
-</article>
+<div class="card-columns">
+    <div class="card">
+        <h3>Health</h3>
+        <ul>
+            <li><a href="/articles/firstaid/">First aid for rats</a></li>
+            <li><a href="/articles/metacam/">Metacam</a></li>
+            <li><a href="/articles/steroids/">Steroids</a></li>
+            <li><a href="/articles/quarantine/">Quarantining rats</a></li>
+            <li><a href="/articles/vets/">Recommended vets</a></li>
+            <li><a href="/articles/infectionsafepractice/">Infection and safe practice</a></li>
+        </ul>
+    </div>
 
-<article>
-    <h3>Habitat &amp; husbandry</h3>
-    <ul>
-        <li><a href="/articles/ratseyeview/">Furnishing a cage - a rat's eye view</a></li>
-    </ul>
-</article>
+    <div class="card">
+        <h3>Training &amp; behaviour</h3>
+        <ul>
+            <li><a href="/articles/agilitytraining/">Agility training</a></li>
+            <li><a href="/articles/training/">Training your rat</a></li>
+        </ul>
+    </div>
 
-<article>
-    <h3>Other UK rat clubs *</h3>
-    <ul>
-        <li><a href="http://www.nfrs.org/" target="_blank">National Fancy Rat Society</a></li>
-        <li><a href="http://www.yorkshireratclub.org.uk/" target="_blank">Yorkshire Rat Club</a></li>
-        <li><a href="http://www.midlandsratclub.org/" target="_blank">Midlands Rat Club</a></li>
-    </ul>
-</article>
+    <div class="card">
+        <h3>Habitat &amp; husbandry</h3>
+        <ul>
+            <li><a href="/articles/ratseyeview/">Furnishing a cage - a rat's eye view</a></li>
+        </ul>
+    </div>
 
-<article>
-    <h3>Forums and groups *</h3>
-    <ul>
-        <li><a href="http://www.fancyratsforum.co.uk/" target="_blank">Fancy Rats forum</a></li>
-        <li><a href="https://www.facebook.com/groups/RatCareUK/" target="_blank">Rat Care UK</a></li>
-        <li><a href="https://www.facebook.com/groups/RRNUK" target="_blank">Rat Rescue Network UK</a></li>    
-    </ul>
-</article>
+    <div class="card">
+        <h3>Other UK rat clubs *</h3>
+        <ul>
+            <li><a href="http://www.nfrs.org/" target="_blank">National Fancy Rat Society</a></li>
+            <li><a href="http://www.yorkshireratclub.org.uk/" target="_blank">Yorkshire Rat Club</a></li>
+            <li><a href="http://www.midlandsratclub.org/" target="_blank">Midlands Rat Club</a></li>
+        </ul>
+    </div>
 
-<article>
-    <h3>General care &amp; information</h3>
-    <ul>
-        <li><a href="/articles/company/">Why rats need company</a></li>
-        <li><a href="/articles/petshops/">Buying rats from petshop</a></li>
-        <li><a href="/resources/docs/NERSratcare.pdf" target="_blank">NERS Rat Care guide [PDF]</a></li>
-    </ul>
-</article>
+    <div class="card">
+        <h3>Forums and groups *</h3>
+        <ul>
+            <li><a href="http://www.fancyratsforum.co.uk/" target="_blank">Fancy Rats forum</a></li>
+            <li><a href="https://www.facebook.com/groups/RatCareUK/" target="_blank">Rat Care UK</a></li>
+            <li><a href="https://www.facebook.com/groups/RRNUK" target="_blank">Rat Rescue Network UK</a></li>    
+        </ul>
+    </div>
 
-<article>
-    <h3>Rat Cafe</h3>
-    <ul>
-        <li><a href="/articles/fatnessfitness/">From fatness to fitness</a></li>
-        <li><a href="/articles/feedingforcondition/">Feeding for condition</a></li>
-        <li><a href="/articles/autumn/">Autumnal revelries</a></li>
-        <li><a href="/articles/keepingcool/">Keeping cool</a></li>
-    </ul>
-</article>
+    <div class="card">
+        <h3>General care &amp; information</h3>
+        <ul>
+            <li><a href="/articles/company/">Why rats need company</a></li>
+            <li><a href="/articles/petshops/">Buying rats from petshop</a></li>
+            <li><a href="/resources/docs/NERSratcare.pdf" target="_blank">NERS Rat Care guide [PDF]</a></li>
+        </ul>
+    </div>
 
-<article>
-    <h3>Other rat-related websites *</h3>
-    <ul>
-        <li><a href="http://www.ratrations.com/" target="_blank">RatRations</a></li>
-        <li><a href="http://ratguide.com/" target="_blank">Rat Guide</li>
-        <li><a href="http://www.cosybedsandburrows.com/" target="_blank">Cosy Beds and Burrows</a></li>
-        <li><a href="http://www.bettysbeds.com/" target="_blank">Betty's Beds</a></li>
-        <li><a href="http://www.ratwarehouse.com/" target="_blank">The Ratwarehouse</a></li>
-        <li><a href="http://www.ratbehavior.org/" target="_blank">Ratbehavior.org</a></li>
-        <li><a href="http://scarlettsparrotessentials.co.uk/" target="_blank">Scarletts Parrot Essentials</a></li>
-        <li><a href="http://www.northernparrots.com/" target="_blank">Northern Parrots</a></li>
-        <li><a href="http://www.ratsratsrats.co.uk/" target="_blank">RatsRatsRats</a></li>
-        <li><a href="http://www.fuzzbutt.co.uk/" target="_blank">Fuzzbutts</a></li>
-        <li><a href="http://www.dapper.com.au/toys.htm" target="_blank">The Dapper Rat</a></li>
-        <li><a href="http://www.dustfreehay.co.uk/" target="_blank">DustFreeHay</a></li>
-        <li><a href="http://www.thehayexperts.co.uk/" target="_blank">The Hay Experts</a></li>
-    </ul>
-</article>
+    <div class="card">
+        <h3>Rat Cafe</h3>
+        <ul>
+            <li><a href="/articles/fatnessfitness/">From fatness to fitness</a></li>
+            <li><a href="/articles/feedingforcondition/">Feeding for condition</a></li>
+            <li><a href="/articles/autumn/">Autumnal revelries</a></li>
+            <li><a href="/articles/keepingcool/">Keeping cool</a></li>
+        </ul>
+    </div>
 
-<article>
-    <h3>Show articles</h3>
-    <ul>
-        <li><a href="/articles/preventinginfection/">Preventing show-related infection</a></li>
-        <li><a href="/articles/showgeneral/">General show information</a></li>
-        <li><a href="/articles/showpreparation/">Show preparation</a></li>
-        <li><a href="/articles/howtoshow/">How to show your rats</a></li>
-        <li><a href="/articles/petjudging/">What do pet judges look for?</a></li>
-        <li><a href="/articles/rosettes/">Rosettes and trophies</a></li>
-        <li><a href="/articles/championships/">Championships</a></li>
-        <li><a href="/articles/funevents/">Fun events</a></li>
-        <li><a href="/show/results/">Show results archive</a></li>
-        <li><a href="/articles/showphotos/">Show photos</a></li>
-        <li><a href="/resources/docs/stewardingmanual.pdf" target="_blank">NERS stewarding booklet [PDF]</a></li>
-    </ul>
-</article>
+    <div class="card">
+        <h3>Other rat-related websites *</h3>
+        <ul>
+            <li><a href="http://www.ratrations.com/" target="_blank">RatRations</a></li>
+            <li><a href="http://ratguide.com/" target="_blank">Rat Guide</li>
+            <li><a href="http://www.cosybedsandburrows.com/" target="_blank">Cosy Beds and Burrows</a></li>
+            <li><a href="http://www.bettysbeds.com/" target="_blank">Betty's Beds</a></li>
+            <li><a href="http://www.ratwarehouse.com/" target="_blank">The Ratwarehouse</a></li>
+            <li><a href="http://www.ratbehavior.org/" target="_blank">Ratbehavior.org</a></li>
+            <li><a href="http://scarlettsparrotessentials.co.uk/" target="_blank">Scarletts Parrot Essentials</a></li>
+            <li><a href="http://www.northernparrots.com/" target="_blank">Northern Parrots</a></li>
+            <li><a href="http://www.ratsratsrats.co.uk/" target="_blank">RatsRatsRats</a></li>
+            <li><a href="http://www.fuzzbutt.co.uk/" target="_blank">Fuzzbutts</a></li>
+            <li><a href="http://www.dapper.com.au/toys.htm" target="_blank">The Dapper Rat</a></li>
+            <li><a href="http://www.dustfreehay.co.uk/" target="_blank">DustFreeHay</a></li>
+            <li><a href="http://www.thehayexperts.co.uk/" target="_blank">The Hay Experts</a></li>
+        </ul>
+    </div>
 
-<article>
-    <h3>Standards, show rules, etc</h3>
-    <ul>
-        <li><a href="https://www.nfrs.org/docs/NFRS_Standards_of_Excellence.pdf" target="_blank">NFRS variety standards [PDF]</a></li>
-        <li><a href="/resources/docs/petstandards.pdf" target="_blank">NERS pet standards [PDF]</a></li>
-        <li><a href="/articles/varietiesschedule/">Show schedule - Varieties</a></li>
-        <li><a href="/articles/petsschedule/">Show schedule - Pets</a></li>
-        <li><a href="/articles/showrules/">Show rules</a></li>
-        <li><a href="/articles/guidancenotes/">Guidance notes for the show entry form</a></li>
-        <li><a href="/resources/docs/petjudgingmanual.pdf" target="_blank">NERS Pet Judging manual [PDF]</a></li>
-        <li><a href="/resources/docs/agilitymanual.pdf" target="_blank">NERS Agility Judging manual [PDF]</a></li>
-    </ul>
-</article>
+    <div class="card">
+        <h3>Show articles</h3>
+        <ul>
+            <li><a href="/articles/preventinginfection/">Preventing show-related infection</a></li>
+            <li><a href="/articles/showgeneral/">General show information</a></li>
+            <li><a href="/articles/showpreparation/">Show preparation</a></li>
+            <li><a href="/articles/howtoshow/">How to show your rats</a></li>
+            <li><a href="/articles/petjudging/">What do pet judges look for?</a></li>
+            <li><a href="/articles/rosettes/">Rosettes and trophies</a></li>
+            <li><a href="/articles/championships/">Championships</a></li>
+            <li><a href="/articles/funevents/">Fun events</a></li>
+            <li><a href="/show/results/">Show results archive</a></li>
+            <li><a href="/articles/showphotos/">Show photos</a></li>
+            <li><a href="/resources/docs/stewardingmanual.pdf" target="_blank">NERS stewarding booklet [PDF]</a></li>
+        </ul>
+    </div>
 
-<article>
-    <h3>NERS members' websites *</h3>
-    <ul>
-        <li><a href="https://shunamiterats.co.uk/" target="_blank">The Scuttling Gourmet</a></li>
-        <li><a href="http://ratvarieties.com/" target="_blank">Hawthorn Rat Varieties</a></li>
-        <li><a href="http://www.isamurats.co.uk/" target="_blank">Isamu Rats</a></li>
-        <li><a href="http://www.batrats.co.uk/" target="_blank">The BatRats</a></li>
-    </ul>
-</article>
+    <div class="card">
+        <h3>Standards, show rules, etc</h3>
+        <ul>
+            <li><a href="https://www.nfrs.org/docs/NFRS_Standards_of_Excellence.pdf" target="_blank">NFRS variety standards [PDF]</a></li>
+            <li><a href="/resources/docs/petstandards.pdf" target="_blank">NERS pet standards [PDF]</a></li>
+            <li><a href="/articles/varietiesschedule/">Show schedule - Varieties</a></li>
+            <li><a href="/articles/petsschedule/">Show schedule - Pets</a></li>
+            <li><a href="/articles/showrules/">Show rules</a></li>
+            <li><a href="/articles/guidancenotes/">Guidance notes for the show entry form</a></li>
+            <li><a href="/resources/docs/petjudgingmanual.pdf" target="_blank">NERS Pet Judging manual [PDF]</a></li>
+            <li><a href="/resources/docs/agilitymanual.pdf" target="_blank">NERS Agility Judging manual [PDF]</a></li>
+        </ul>
+    </div>
+
+    <div class="card">
+        <h3>NERS members' websites *</h3>
+        <ul>
+            <li><a href="https://shunamiterats.co.uk/" target="_blank">The Scuttling Gourmet</a></li>
+            <li><a href="http://ratvarieties.com/" target="_blank">Hawthorn Rat Varieties</a></li>
+            <li><a href="http://www.isamurats.co.uk/" target="_blank">Isamu Rats</a></li>
+            <li><a href="http://www.batrats.co.uk/" target="_blank">The BatRats</a></li>
+        </ul>
+    </div>
+    
+</div>
 
 <footnote>
     <p>* External links are reviewed when the link is published. However, NERS cannot be held responsible for the content or accuracy of external websites. Please note that inclusion on the list does not constitute a recommendation or endorsement by the society.</p>
